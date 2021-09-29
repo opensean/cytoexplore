77d1858e19014170ec29294ad3f7ed785317cce0
Sean Landry <20714286+opensean@users.noreply.github.com>
2021-09-28 20:50:06 -0400

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,140 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container id="cy" fluid class="fill-height">
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import cytoscape from 'cytoscape';
+  import nodeHtmlLabel from 'cytoscape-node-html-label';
+  import ApexCharts from "apexcharts";
+  import dagre from 'cytoscape-dagre';
+  
+  cytoscape.use(dagre);
+  nodeHtmlLabel( cytoscape );
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      cy: null,
+    }),
+    mounted: function(){
+      // Select the node that will be observed for mutations
+      const targetNode = document.getElementById('cy');
+      
+      // Options for the observer (which mutations to observe)
+      const config = { attributes: true, childList: true, subtree: true };
+
+      const observer = new MutationObserver(this.filterEvents);
+      observer.observe(targetNode, config);
+
+      var cy = cytoscape({
+
+        container: document.getElementById('cy'), // container to render in
+        elements: {
+           nodes:[
+             { data: { id: 'a', value: 70, value2: 50} },
+             { data: { id: 'b', value: 90, value2: 50} },
+             { data: { id: 'c', value: 90, value2: 50} },
+             { data: { id: 'd', value: 90, value2: 50} },
+             { data: { id: 'e', value: 90, value2: 50} },
+             { data: { id: 'f', value: 90, value2: 50} },
+           ],
+           edges:[
+           {data: {id: 'ab', source: 'a', target: 'b'}},
+           {data: {id: 'ac', source: 'a', target: 'c'}},
+           {data: {id: 'bd', source: 'b', target: 'd'}},
+           {data: {id: 'cf', source: 'c', target: 'f'}},
+           {data: {id: 'ce', source: 'c', target: 'e'}}
+           ]
+         },
+         style: [ // the stylesheet for the graph
+           {
+             selector: 'node',
+             style: {
+               'background-color': '#666',
+             }
+           },
+
+           {
+             selector: 'edge',
+             style: {
+               'width': 3,
+               'line-color': '#ccc',
+               'target-arrow-color': '#ccc',
+               'target-arrow-shape': 'triangle',
+               'curve-style': 'bezier'
+             }
+           }
+         ],
+         layout: {
+           name: 'dagre',
+           spacingFactor: 3,
+           rankDir: "LR"
+         }
+
+      });
+      cy.viewport({zoom:0.75});
+      cy.center();
+      //cy.layout({ name: 'klay'}).run()
+      cy.nodeHtmlLabel([
+        {
+          query: 'node', // cytoscape query selector
+          halign: 'center', // title vertical position. Can be 'left',''center, 'right'
+          valign: 'center', // title vertical position. Can be 'top',''center, 'bottom'
+          halignBox: 'center', // title vertical position. Can be 'left',''center, 'right'
+          valignBox: 'top', // title relative box vertical position. Can be 'top',''center, 'bottom'
+          cssClass: '', // any classes will be as attribute of <div> container for every title
+          tpl(data) {
+            return '<div id="renderNodePlot-' + data.id +'"></div>'
+          }
+        }
+      ]);
+    },
+
+    methods: {
+      //filterEvents: function (mutationsList, observer) {
+      filterEvents: function (mutationsList) {
+        for(const mutation of mutationsList) {
+            //console.log(mutation);
+            if (mutation.type === 'childList') {
+                //console.log('A child node has been added or removed.');
+                for(const node of mutation.addedNodes){
+                  if(node.id && node.id.startsWith("renderNodePlot-")){
+                    this.renderPlot(node.id);
+                  }
+                }
+            }
+         }
+       },
+       renderPlot: function(id) {
+         //console.log("rendering plot for " + id.split("renderNodePlot-")[1]);
+         // use the result of id.split("renderNodePlot-") to find node data in elements
+         var options1 = {
+                chart: {
+                  width: 200,
+                  type: "radialBar",
+                },
+                series: [67, 84, 97, 61],
+                plotOptions: {
+                  radialBar: {
+                    dataLabels: {
+                      total: {
+                        show: true,
+                        label: id.split("renderNodePlot-")[1]
+                      }
+                    }
+                  }
+                },
+                labels: ['TEAM A', 'TEAM B', 'TEAM C', 'TEAM D']
+          };
+          return new ApexCharts(document.getElementById(id), options1).render();
+      }
+    },
+
+    updated: function(){
+
+    }
+
+  }
+</script>
---
+++ src/components/VProgressCircularOverlay.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-progress-circular value="90" color="red"/>
+    <div style="position:absolute;top:0,left:0,z-index:9">
+      <v-progress-circular value="70" color="blue"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  //import { VProgressCircular } from "vuetify/lib";
+
+  export default {
+    name: 'VProgressCircularOverlay',
+
+    data: () => ({
+    }),
+    mounted: function(){
+    }
+
+  }
+</script>
